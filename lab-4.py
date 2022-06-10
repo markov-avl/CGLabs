c017190d963f991d4b1cbf6071d1e77ac13bc894
markov-avl <andrewshasucks@gmail.com>
2022-06-10 19:46:04 +1000

    lab-4

--- lab-4.py
+++ lab-4.py
@@ -1,39 +1,113 @@
-import cv2
 import numpy as np
-from PIL import ImageDraw, Image
+from PIL import Image
+from PIL.ImageDraw import Draw
 
 
-def bilinear_interpolate(im, x, y):
-    x = np.asarray(x)
-    y = np.asarray(y)
+def quad_as_rect(quad):
+    return not (quad[0] != quad[2] or quad[1] != quad[7] or quad[4] != quad[6] or quad[3] != quad[5])
 
-    x0 = np.floor(x).astype(int)
-    x1 = x0 + 1
-    y0 = np.floor(y).astype(int)
-    y1 = y0 + 1
 
-    x0 = np.clip(x0, 0, im.shape[1] - 1)
-    x1 = np.clip(x1, 0, im.shape[1] - 1)
-    y0 = np.clip(y0, 0, im.shape[0] - 1)
-    y1 = np.clip(y1, 0, im.shape[0] - 1)
+def quad_to_rect(quad):
+    assert len(quad) == 8
+    assert quad_as_rect(quad)
+    return quad[0], quad[1], quad[4], quad[3]
 
-    print(x0, x1, y0, y1)
-    Ia = im[y0, x0]
-    Ib = im[y1, x0]
-    Ic = im[y0, x1]
-    Id = im[y1, x1]
 
-    wa = (x1 - x) * (y1 - y)
-    wb = (x1 - x) * (y - y0)
-    wc = (x - x0) * (y1 - y)
-    wd = (x - x0) * (y - y0)
+def rect_to_quad(rect):
+    assert len(rect) == 4
+    return rect[0], rect[1], rect[0], rect[3], rect[2], rect[3], rect[2], rect[1]
 
-    print(wa, wb, wc, wd)
-    print(Ia, Ib, Ic, Id)
 
-    return wa * Ia + wb * Ib + wc * Ic + wd * Id
+# разбивает на части (сетка)
+def griddify(xy: tuple[int, int, int, int], rows: int, colums: int, ):
+    h = xy[3] - xy[1]
+    w = xy[2] - xy[0]
+
+    # шагаем (делим картинку по частям)
+    x_step = w / colums
+    y_step = h / rows
+
+    y = xy[1]
+    grid_vertex_matrix = []
+    for _ in range(rows + 1):
+        grid_vertex_matrix.append([])
+        x = xy[0]
+        for _ in range(colums + 1):
+            grid_vertex_matrix[-1].append([int(x), int(y)])
+            x += x_step
+        y += y_step
+
+    # разбили изображение на сетку
+    grid = np.array(grid_vertex_matrix)
+    return grid
+
+
+# рандомизируем разбитые части на основе полученных (искажаем сетку)
+def distort_grid(grid: np.ndarray, max_shift: int):
+    new_grid = np.copy(grid)
+
+    # находим границы рисунка
+    x_min = np.min(new_grid[:, :, 0])
+    y_min = np.min(new_grid[:, :, 1])
+    x_max = np.max(new_grid[:, :, 0])
+    y_max = np.max(new_grid[:, :, 1])
+
+    # new_grid.shape - вернёт количество массивов, подмассивов, элеиентов в подмассивах
+    new_grid += np.random.randint(- max_shift, max_shift + 1, new_grid.shape)
+    new_grid[:, :, 0] = np.maximum(x_min, new_grid[:, :, 0])
+    new_grid[:, :, 1] = np.maximum(y_min, new_grid[:, :, 1])
+    new_grid[:, :, 0] = np.minimum(x_max, new_grid[:, :, 0])
+    new_grid[:, :, 1] = np.minimum(y_max, new_grid[:, :, 1])
+    return new_grid
+
+
+def grid_to_mesh(src_grid, dst_grid):
+    mesh = []
+    for i in range(src_grid.shape[0] - 1):
+        for j in range(src_grid.shape[1] - 1):
+            src_quad = [src_grid[i, j, 0], src_grid[i, j, 1],
+                        src_grid[i + 1, j, 0], src_grid[i + 1, j, 1],
+                        src_grid[i + 1, j + 1, 0], src_grid[i + 1, j + 1, 1],
+                        src_grid[i, j + 1, 0], src_grid[i, j + 1, 1]]
+            dst_quad = [dst_grid[i, j, 0], dst_grid[i, j, 1],
+                        dst_grid[i + 1, j, 0], dst_grid[i + 1, j, 1],
+                        dst_grid[i + 1, j + 1, 0], dst_grid[i + 1, j + 1, 1],
+                        dst_grid[i, j + 1, 0], dst_grid[i, j + 1, 1]]
+            dst_rect = quad_to_rect(dst_quad)
+            mesh.append([dst_rect, src_quad])
+    return mesh
+
+
+def draw_grid(image: Image, grid: np.ndarray) -> None:
+    drawer = Draw(image)
+    for x in range(grid.shape[0] - 1):
+        for y in range(grid.shape[1] - 1):
+            for dx in (0, 1):
+                for dy in (0, 1):
+                    if dx == 0 and dy == 0:
+                        continue
+                    drawer.line((*grid[x][y], *grid[x + dx][y + dy]), fill='red')
 
 
 if __name__ == '__main__':
-    im = cv2.imread("source/image-1.png")
-    print(bilinear_interpolate(im, [2, 4], [5, 9]))
+    image = Image.open('source/image-2.png')
+    rows = 10
+    columns = 5
+
+    grid = griddify((0, 0, image.width, image.height), rows, columns)
+    image_with_grid = image.copy()
+    draw_grid(image_with_grid, grid)
+    image_with_grid.save('result/warping-before.png')
+
+    distorted_grid = distort_grid(grid, 30)
+    mesh = grid_to_mesh(distorted_grid, grid)
+
+    correctly_distorted_grid = grid.copy()
+    for i in range(correctly_distorted_grid.shape[0] - 1):
+        for j in range(correctly_distorted_grid.shape[1] - 1):
+            correctly_distorted_grid[i][j][0] += grid[i][j][0] - distorted_grid[i][j][0]
+            correctly_distorted_grid[i][j][1] += grid[i][j][1] - distorted_grid[i][j][1]
+
+    image_with_distorted_grid = image.transform(image.size, Image.MESH, mesh, Image.BILINEAR)
+    draw_grid(image_with_distorted_grid, correctly_distorted_grid)
+    image_with_distorted_grid.save('result/warping-after.png')
